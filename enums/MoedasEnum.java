07c68a6fadfb34f6f66a2a72289884eb34ac55e9
Emerson Penelli <132641090+EmersonPenelli@users.noreply.github.com>
2024-06-04 21:50:25 -0300

    feat: adicionando novas funções

--- enums/MoedasEnum.java
+++ enums/MoedasEnum.java
@@ -1,31 +1,10 @@
-package enums;
-
-public enum MoedasEnum {
-
-    BRL("Real Brasileiro", "BRL"), 
-    USD("Dólar Americano", "USD"), 
     EUR("Euro", "EUR"), 
     GBP("Libra Esterlina", "GBP"), 
     ARS("Peso Argentino", "ARS"), 
-    CLP("Peso Chileno", "CLP"), 
-    CNY("Yuan Chinês", "CNY"), 
-    JPY("Iene Japonês", "JPY"), 
+    CLP("Peso Chileno", "CLP"),
+    JPY("Iene Japonês", "JPY"),
+    CNY("Yuan Chinês", "CNY"),
     KRW("Won Sul-Coreano", "KRW");
 
     private final String nome;
     private final String sigla;
-
-    MoedasEnum(String nome, String sigla) {
-        this.nome = nome;
-        this.sigla = sigla;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getSigla() {
-        return sigla;
-    }
-
-}
