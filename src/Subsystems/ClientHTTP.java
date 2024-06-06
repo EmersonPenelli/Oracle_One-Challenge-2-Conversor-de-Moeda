f675809f93d57241652ec89155e319f1340ef53b
Emerson Penelli <132641090+EmersonPenelli@users.noreply.github.com>
2024-06-06 20:38:48 -0300

    Finalizando o projeto

---
+++ src/Subsystems/ClientHTTP.java
@@ -0,0 +1,34 @@
+package subsystems;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+import javax.swing.JOptionPane;
+
+import res.Global;
+
+public class ClientHTTP {
+
+    public String findData(String url) {
+
+        URI address = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(address).GET().build();
+
+        HttpResponse<String> response;
+
+        try {
+            response = client.send(request, BodyHandlers.ofString());
+            return response.body();
+        } catch (IOException | InterruptedException ex) {
+            ex.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Erro de conexão.", Global.TITULO, 0);
+            throw new RuntimeException("Erro ao buscar URL ou sem acesso a internet.");
+        }
+
+    }
+}
