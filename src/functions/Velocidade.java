396db57bb49aea266413e744df463eb3f884c942
Emerson Penelli <132641090+EmersonPenelli@users.noreply.github.com>
2024-06-06 21:05:04 -0300

    Finalizando o projeto

--- src/functions/Velocidade.java
+++ src/functions/Velocidade.java
@@ -8,11 +8,18 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-import Subsystens.VelCalculos;
+import subsystens.VelCalculos;
 import interfaces.Conversores;
 import res.Global;
 
-        public void conversor() {
+public class Velocidade implements Conversores {
+
+    @Override
+    public void conversor() {
+
+        double valorDigitado;
+        String opcao1Selecionada;
+        String opcao2Selecionada;
         int botaoPressionado;
         double resultado = 0;
 
@@ -20,54 +27,57 @@ import res.Global;
 
         // Construção do quadro de opções
 
-        String[] botao = { "Converter" };
-        String[] botoesConfirmacao = { "Sim", "Não" };
-        String[] opcoes = { "km/h", "mph", "mn", "Ma" };
-        String avisoMensagem = "Apenas números podem ser inseridos.\nPara inserir casas decimais utilize ponto (.)";
-        final String[] BOTAO = { "Converter" };
-        final String[] BOTOES_CONFIRMACAO = { "Sim", "Não" };
         final String[] OPCOES = { "km/h", "mph", "mn/h", "Ma" };
-        final String AVISO_MENSAGEM = "Apenas números podem ser inseridos.\nPara inserir casas decimais utilize ponto (.)";
 
         JPanel quadro = new JPanel();
         JLabel txt1 = new JLabel("Converter ");
         JTextField valor = new JTextField(3);
         JLabel txt2 = new JLabel(" para ");
-        JComboBox<String> opcoesLista1 = new JComboBox<String>(opcoes);
-        JComboBox<String> opcoesLista2 = new JComboBox<String>(opcoes);
         JComboBox<String> opcoesLista1 = new JComboBox<String>(OPCOES);
         JComboBox<String> opcoesLista2 = new JComboBox<String>(OPCOES);
         JLabel txt3 = new JLabel(
                 "<html><body width='%1s'><p>km/h - Quilômentros por hora<br>mph - Milhas por hora<br>mn - Nós (Milhas náutica por hora)<br>Ma - Número de Mach</p>");
 
-            public void conversor() {
+        quadro.add(txt1);
+        quadro.add(valor);
+        quadro.add(opcoesLista1);
+        quadro.add(txt2);
+        quadro.add(opcoesLista2);
+        quadro.add(txt3);
+
+        // Início da função
+
+        while (true) {
+
+            // Exibir quadro e coletar dados do usuário
 
             while (true) {
                 botaoPressionado = JOptionPane.showOptionDialog(null, quadro, Global.TITULO, JOptionPane.NO_OPTION,
-                        JOptionPane.QUESTION_MESSAGE, null, botao, null);
-                        JOptionPane.QUESTION_MESSAGE, null, BOTAO, null);
-
+                        JOptionPane.QUESTION_MESSAGE, null, Global.BOTAO, null);
+                
                 if (botaoPressionado == 0) { // Botão "CONVERTER" pressionado
 
-            public void conversor() {
+                    try {
+                        valorDigitado = Double.valueOf(valor.getText());
+                        opcao1Selecionada = (String) opcoesLista1.getSelectedItem();
                         opcao2Selecionada = (String) opcoesLista2.getSelectedItem();
                         break;
                     } catch (NumberFormatException e) {
-                        JOptionPane.showMessageDialog(null, avisoMensagem, Global.TITULO, 0);
-                        JOptionPane.showMessageDialog(null, AVISO_MENSAGEM, Global.TITULO, 0);
+                        JOptionPane.showMessageDialog(null, Global.AVISO_MENSAGEM, Global.TITULO, 0);
                         valor.setText("");
                     }
-
-            public void conversor() {
+                
+                } else { // Botão "FECHAR" pressionado
+                    throw new RuntimeException("Função encerrada.");
+                }
+            }
 
             // Calcular resultados
 
-
             VelCalculos vcalc = new VelCalculos();
             resultado = vcalc.calculos(valorDigitado, opcao1Selecionada, opcao2Selecionada, OPCOES);
-
+            
             // Exibir resultado
-
 
             String resultadoFinal = formato.format(valorDigitado) + " " + opcao1Selecionada + " é igual a "
                     + formato.format(resultado) + " " + opcao2Selecionada + ".";
@@ -76,15 +86,12 @@ import res.Global;
             // Continuar ou sair da função
 
             botaoPressionado = JOptionPane.showOptionDialog(null, "Deseja fazer outra conversão?", Global.TITULO, 0, 3,
-                    null, botoesConfirmacao, null);
-                    null, BOTOES_CONFIRMACAO, null);
+                    null, Global.BOTOES_CONFIRMACAO, null);
 
             if (botaoPressionado != 0) { // Sair
                 break;
             } else { // Limpar campos antes de recomeçar
                 valor.setText("");
-                opcoesLista1.setSelectedItem(opcoes[0]);
-                opcoesLista2.setSelectedItem(opcoes[0]);
                 opcoesLista1.setSelectedItem(OPCOES[0]);
                 opcoesLista2.setSelectedItem(OPCOES[0]);
             }
